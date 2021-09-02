9183c46bf5c0e0b5fca3f5fcea220e713be8ec7f
khalyl-hamdy <khalyl.hm@gmail.com>
2021-09-02 22:18:53 +0100

    add api-gateawy and eureka server projects and change some configs

--- currency-conversion-service/src/main/java/com/training/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/training/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -12,12 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.training.microservices.currencyconversionservice.bean.CurrencyConversion;
+import com.training.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
 
 	@Autowired
-	Environment env;
+	private CurrencyExchangeProxy proxy;
 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
@@ -32,13 +33,30 @@ public class CurrencyConversionController {
 		uriVariables.put("to",to);
 		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity
 				("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
-						CurrencyConversion.class, uriVariables);		currencyConversion.setEnvironment(env.getProperty("local.server.port"));
+						CurrencyConversion.class, uriVariables);
 						currencyConversion=responseEntity.getBody();
 						return new CurrencyConversion(currencyConversion.getId(), 
 								from, to, quantity, 
 								currencyConversion.getConversionMultiple(), 
 								quantity.multiply(currencyConversion.getConversionMultiple()), 
 								currencyConversion.getEnvironment()+ " " + "rest template");
+		
+	}
+	
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+				
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment() + " " + "feign");
 		
 	}
 
